758ee9764c6c5f36c0069e094b1097a343f2e87a
Roma <romabelka43@gmail.com>
2023-02-01 19:27:47 +0300

    validation and mapping initial implementation

---
+++ Validation/Validators/FarmValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+using Models.Core;
+
+namespace Validation.Validators;
+
+public class FarmValidator : AbstractValidator<FarmDto>
+{
+	public FarmValidator()
+	{
+		RuleFor(x => x.Name).NotEmpty().WithMessage("Farm name is required");
+	}
+}
